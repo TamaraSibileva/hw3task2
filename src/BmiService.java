08ad21b6479839f696f235a30019e5bb6765aa49
Tamara Sibileva <tomav93@mail.ru>
2024-12-15 16:42:13 +1200

    Changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
 
-    public double calculate(double weightKg, double heightM) {
+    public int calculate(double weightKg, double heightM) {
         double index = weightKg / heightM / heightM;
         int result;
         result = (int) index;
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         BmiService service = new BmiService();
         double weightKg = 98; // вес в кг
         double heightM = 1.87; // рост в метрах
-        int bmi = (int) service.calculate(weightKg, heightM);
+        int bmi = service.calculate(weightKg, heightM);
         System.out.println("bmi-индекс: " + bmi);
     }
 }
